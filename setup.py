03427e41de3ba03495e60670c441c57f193e37d4
Junyi Ren <jyren9871@gmail.com>
2023-06-06 17:28:20 +0800

    Update build system

--- setup.py
+++ setup.py
@@ -5,18 +5,7 @@ import os
 
 __version__ = "0.0.1"
 
-# submodules:
-#     cartesian_mod -> src/coords/cartesian_mod_core.cpp
-#         install to: pyfefi.coords.lib.cartesian_mod
-#     interp -> src/interp/interp.cpp
-#         install to: pyfefi.interp
-# main module:
-#     pyfefi -> pyfefi (pure python)
-#         install to: pyfefi
-# cxx includes:
-#     include
-# cxx compile flags:
-#     -std=c++20 -O3 -march=native -fopenmp
+headers = glob("include/*")
 
 cxx_config = dict(
     include_dirs = ["include"],
@@ -47,12 +36,9 @@ setup(
     author="JyRen",
     author_email="j.y.ren9871@gmail.com",
     url="",
-    description="Python wrapper for FEFF interface",
+    description="Python post processer for fefi code",
     long_description="",
     ext_modules=ext_modules,
-    install_requires=[
-        "numpy", "scipy", "pybind11", "pyvista", "numba", "matplotlib", "netCDF4", "f90nml", "pandas"
-    ],
     setup_requires=["pybind11>=2.5.0"],
     cmdclass={"build_ext": build_ext},
     packages=packages,
